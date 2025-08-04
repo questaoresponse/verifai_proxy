6bb1b50d9f7637f45b5db01ad7a5e3833849861e
verificaicode <oiiaiiocat@gmail.com>
2025-08-04 16:11:34 -0300

    update

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ async def loop():
             pass
 
 def run_flask():
-    io.run(app, "0.0.0.0", port=12345)
+    io.run(app, "0.0.0.0", port=12345, debug=True)
     
 async def main():
     flask_thread = Thread(target=run_flask)
